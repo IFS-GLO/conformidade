7f6186a48a126ede5b4bc2d21b13c4d96d0e4013
Raphael F <raphael.fontes@ifs.edu.br>
2018-01-23 09:33:49 -0300

    Criação do app core

---
+++ frigga/frigga/core/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Fund(models.Model):
+    name = models.CharField(max_length=15, verbose_name='Nome')
+
+    class Meta:
+        verbose_name = 'Fundo'
+
+
+class Unit(models.Model):
+    name = models.CharField(max_length=120, verbose_name='Nome')
+
+    class Meta:
+        verbose_name = 'Unidade'
+
+
+class Specie(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome')
+
+    class Meta:
+        verbose_name = 'Espécie'
+
+
+class Conformity(models.Model):
+    document_number = models.CharField(max_length=25, verbose_name='Número do documento')
+    subject = models.CharField(max_length=120, verbose_name='Assunto')
+    classification_code = models.CharField(max_length=45, verbose_name='Código de classificação')
+    production_date = models.DateField(verbose_name='Data de produção')
+    payment_date = models.DateField(verbose_name='Data de pagamento')
+    box = models.IntegerField(verbose_name='Caixa')
+    note = models.TextField(verbose_name='Observação')
+
+    fund = models.ForeignKey(Fund, verbose_name=Fund._meta.verbose_name)
+    unit = models.ForeignKey(Unit, verbose_name=Unit._meta.verbose_name)
